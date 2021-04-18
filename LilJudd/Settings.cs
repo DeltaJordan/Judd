f03ad8c627179cfca603eff9a144b7c425ad0a1d
Jordan Marine <zeotni.jordan@gmail.com>
2021-04-17 22:42:14 -0500

    Add Lil Judd Project. Add settings.json to .gitignore Began development for uptime tracking.

---
+++ LilJudd/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace LilJudd
+{
+    public class Settings
+    {
+        [JsonProperty("bot_token")]
+        public string BotToken { get; set; }
+
+        [JsonProperty("app_key")]
+        public string AppKey { get; set; }
+
+        [JsonProperty("base_id")]
+        public string BaseId { get; set; }
+
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        [JsonProperty("mysql_ip")]
+        public string MySqlIp { get; set; }
+
+        [JsonProperty("mysql_user")]
+        public string MySqlUsername { get; set; }
+
+        [JsonProperty("mysql_pass")]
+        public string MySqlPassword { get; set; }
+
+        public void SaveSettings()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            File.WriteAllText(Path.Combine(Globals.AppPath, "Data", "settings.json"), json);
+        }
+    }
+}
