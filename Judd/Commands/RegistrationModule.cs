a10a3f64950945184680c7ca2b5c47ee77382e24
Davfernape <DGPhilipos@student.fullsail.edu>
2021-04-21 15:24:05 -0400

    made BaseSDLCommandModule
    handles erros, base clase that all command modules should derive from

--- Judd/Commands/RegistrationModule.cs
+++ Judd/Commands/RegistrationModule.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
 using Judd.SQL;
 using MySql.Data.MySqlClient;
 using System;
@@ -10,40 +12,90 @@ using System.Threading.Tasks;
 
 namespace Judd.Commands
 {
-    public class RegistrationModule : BaseCommandModule
+    public class RegistrationModule : BaseSDLCommandModule
     {
         private MySqlConnection mySqlConnection = MySqlClient.MySqlConnection;
 
         [Command("Register")]
         public async Task ApplyForRegistration(CommandContext ctx)
         {
-            //command trigger (reaction / chat input)          
-            //if user does not have profile
-                //dm
-                //rules
-                //ask for profile info: 
+            //get interactivity from user
+            InteractivityModule interactivity = ctx.Client.GetInteractivityModule();
+
+            //if the sql server is not responding (error is handled elsewhere)
+            if (SQLError)
+            {
+                return;
+            }
+            
+            //if the user dm'd the bot
+            if (ctx.Channel.IsPrivate)
+            {
+                //if user does not have profile
+                if (!IsUserRegistered(ctx.User.Id))
+                {
+                    //Begin registration and Display users rules
+                    await ctx.RespondAsync("yo whatup, ty for ur interest. " +
+                        "By continuing you acknowledge that you have read and understand the rules of the server." +
+                        "If you agree and would like to continue your registration," +
+                        "then please respond: Yes"); //TODO: add cancel at any time
+                    //ask for profile info: 
+
                     //IGN, Nickname, FC
                     //extra: Weapons, Bio, Team, idk whatever
-                //Validation:
-                //is valid name 
-                //is valid FC
-                //approval for info tracking idk
-                //connect to splatnet
-                //void CreateTempProfile (player data);
-                //display profile 
-                //user confirmation
-                // void CreateSQLProfile (player data);      
-                // void CreateDiscordProfileFromSQL; 
+                    //Validation:
+                    //is valid name 
+                    //is valid FC
+                    //approval for info tracking idk
+                    //connect to splatnet
+                    //void CreateTempProfile (player data);
+                    //display profile 
+                    //user confirmation
+                    // void CreateSQLProfile (player data);      
+                    // void CreateDiscordProfileFromSQL; 
+                    await ctx.RespondAsync($"{ctx.User} Not registered ");
+                    //INSERT INTO `sys`.`Players` (`discordId`, `nickname`, `friend_code`, `power`) VALUES ('200066011616116737', 'Davfernape', '1', '1');
+                }
                 //else (user has profile) 
-                //ligma balls
-
+                else
+                {
+                    //tell them they are already registered
+                    await ctx.RespondAsync($"{ctx.User} Is already registered");
+                }
+            }
+            //else (the command was NOT in dms where it belongs)
+            else
+            {
+                //tell them they should dm
+                await ctx.RespondAsync($"{ctx.User} read the rules");
+            }
 
             // void CreateSQLProfile (player data);      
             // void CreateDiscordProfileFromSQL; 
             // void CreateTempProfile (player data);
 
         }
-            
+         
+        private bool IsUserRegistered(ulong discordId)
+        {
+            long test = 0;
+            try
+            {
+                MySqlCommand command = new MySqlCommand(
+                    "SELECT EXISTS( SELECT * FROM Players " +
+                    "WHERE discordId=@id)",
+                    mySqlConnection);
+
+                command.Parameters.AddWithValue("@id", discordId);
+                test = (long)command.ExecuteScalar();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return test > 0;
+        }
 
         //////////////
         ////FC CHECK
